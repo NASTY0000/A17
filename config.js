89a67f0f21b142905487f2c88d4a496144abd5bc
NASTY0000 <146393760+NASTY0000@users.noreply.github.com>
2023-09-29 21:06:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["916297175943","919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943","919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.Owner = ["23438944714"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["23438944714"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.OwnerName = "NASTY";
+global.BotName = "NASTY bot";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
